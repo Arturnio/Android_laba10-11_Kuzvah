9e72cbd91b0a208c77f584864868b63a08515812
Artur <only.up.ak@gmail.com>
2026-02-09 13:09:20 +0300

    Step 10: Top App Bar added

--- app/src/main/java/com/arlina/android_laba10_11_kuzvah/MainActivity.kt
+++ app/src/main/java/com/arlina/android_laba10_11_kuzvah/MainActivity.kt
@@ -16,13 +16,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
@@ -33,6 +37,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.arlina.android_laba10_11_kuzvah.data.Student
 import com.arlina.android_laba10_11_kuzvah.data.students
 import com.arlina.android_laba10_11_kuzvah.ui.theme.Android_laba1011_KuzvahTheme
+import com.arlina.android_laba10_11_kuzvah.ui.theme.Montserrat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -106,7 +111,9 @@ fun StudentItem(
 
 @Composable
 fun StudentApp() {
-    Scaffold { it ->
+    Scaffold(
+        topBar = { StudentTopAppBar()}
+    ) { it ->
         LazyColumn(contentPadding = it) {
             items(students) {
                 StudentItem(
@@ -133,4 +140,32 @@ fun StudentDarkThemePreview(){
     Android_laba1011_KuzvahTheme(darkTheme = true) {
         StudentApp()
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun StudentTopAppBar(modifier: Modifier= Modifier){
+    CenterAlignedTopAppBar(
+        title ={
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Image(
+                    modifier = Modifier
+                        .size(dimensionResource(R.dimen.image_size))
+                        .padding(dimensionResource(R.dimen.padding_small)),
+                    painter = painterResource(R.drawable.ic_student_logo),
+                    contentDescription = null
+                )
+                Text(
+                    text = "Student List",
+                    style = MaterialTheme.typography.displayLarge
+                )
+            }
+        },
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        ),
+        modifier = modifier
+    )
 }
